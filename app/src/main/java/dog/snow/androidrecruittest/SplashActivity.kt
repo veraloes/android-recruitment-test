07c92b808932685a3c0c537c4d3e2eebca06cfaf
weronikamichalska <weronikamichalskaa@gmail.com>
2021-06-09 23:06:34 +0200

    add SplashActivity to MainActivity sequence add progressBar

--- app/src/main/java/dog/snow/androidrecruittest/SplashActivity.kt
+++ app/src/main/java/dog/snow/androidrecruittest/SplashActivity.kt
@@ -1,9 +1,52 @@
 package dog.snow.androidrecruittest
 
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import kotlinx.android.synthetic.main.layout_progressbar.*
 
 class SplashActivity : AppCompatActivity(R.layout.splash_activity) {
+
+    private var networkStatus: Boolean = true
+    private var errorMessage: String = "Error"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initProgressBar()
+        checkNetworkConnection()
+        fetchData()
+    }
+
+    private fun checkNetworkConnection() {
+        if (networkStatus) {
+            fetchData()
+        } else {
+            showError(errorMessage)
+        }
+    }
+
+    private fun fetchData() {
+//        TODO(Download all the items)
+
+        Handler().postDelayed(
+            {
+                goToMain()
+            },
+            SPLASH_DISPLAY_LENGTH
+        )
+    }
+
+    private fun initProgressBar() {
+        progressbar.isVisible = true
+    }
+
+    private fun goToMain() {
+        startActivity(Intent(this, MainActivity::class.java))
+    }
+
     private fun showError(errorMessage: String?) {
         MaterialAlertDialogBuilder(this)
             .setTitle(R.string.cant_download_dialog_title)
@@ -13,5 +56,9 @@ class SplashActivity : AppCompatActivity(R.layout.splash_activity) {
             .create()
             .apply { setCanceledOnTouchOutside(false) }
             .show()
+    }
+
+    companion object {
+        private const val SPLASH_DISPLAY_LENGTH: Long = 4000
     }
 }
